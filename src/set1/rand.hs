5307334ab8b4b6ba0053f5299be167464a9d6ef2
Adrian Gonzalez <adrian@healthloop.com>
2016-01-22 19:05:37 -0800

    1.2 Random character generation

--- src/set1/rand.hs
+++ src/set1/rand.hs
@@ -16,3 +16,24 @@ nRands 0 _ = []
 nRands n seed = newRand : nRands (n-1) nextSeed
   where
     newRand@(_, nextSeed) = rand seed
+
+randLetter :: Seed -> (Char, Seed)
+randLetter seed = (toLetter random, nextSeed)
+  where
+    (random, nextSeed) = rand seed
+
+nRandsWithFn :: Integer -> (Seed -> (a, Seed)) -> Seed -> [(a, Seed)]
+nRandsWithFn 0 _ _ = []
+nRandsWithFn n fn seed = newRand : nRandsWithFn (n-1) fn nextSeed
+  where
+    newRand@(_, nextSeed) = fn seed
+
+fiveRands' :: [Integer]
+fiveRands' = map fst $ nRandsWithFn 5 rand initialSeed
+
+randString3 :: String
+randString3 = map fst $ nRandsWithFn 3 randLetter initialSeed
+
+randString3' :: String
+randString3' = map (toLetter . fst) $ nRands 3 initialSeed
+
