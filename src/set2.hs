77634ef97e696f7b4eb288fc10c7cb6ed3ced3fc
Adrian Gonzalez <bernardogzzf@gmail.com>
2016-01-23 19:15:47 -0800

    2.2 Build a library of things that can fail

--- src/set2.hs
+++ src/set2.hs
@@ -11,3 +11,35 @@ instance Show a => Show (Maybe a) where
   show Nothing = "Nothing"
   show (Just a) = "Just " ++ show a
 
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:_) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (_:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay needle ((a,b):xs)
+  | a == needle = Just b
+  | otherwise = lookupMay needle xs
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay a 0 = Nothing
+divMay a b = Just $ a / b
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay (m:[]) = Just m
+maximumMay (m:n:xs)
+  | m > n = maximumMay (m:xs)
+  | otherwise = maximumMay (n:xs)
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay (m:[]) = Just m
+minimumMay (m:n:xs)
+  | m < n = minimumMay (m:xs)
+  | otherwise = minimumMay (n:xs)
