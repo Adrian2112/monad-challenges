7f614722e7885fde4fdfda7672dccf0d21e2d04a
Adrian Gonzalez <bernardogzzf@gmail.com>
2016-01-25 22:28:19 -0800

    3.2 Poker hands

--- src/set3.hs
+++ src/set3.hs
@@ -6,7 +6,12 @@ module Set3 where
 import MCPrelude
 
 allPairs :: [a] -> [b] -> [(a,b)]
-allPairs [] _ = []
-allPairs (x:xs) ys = map ((,) x) ys ++ allPairs xs ys
+allPairs xs ys = concat $ map (\x -> map ((,) x) ys) xs
 
+data Card = Card Int String
 
+instance Show Card where
+  show (Card r s) = show r ++ s
+
+allCards :: [Int] -> [String] -> [Card]
+allCards ranks suits = concat $ map (\rank -> map (Card rank) suits) ranks
