6c7084b391da9a487c47ecac14a0535e805f41c0
Adrian Gonzalez <bernardogzzf@gmail.com>
2016-01-25 21:32:48 -0800

    3.1 generating permutations

---
+++ src/set3.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RebindableSyntax  #-}
+
+module Set3 where
+
+import MCPrelude
+
+allPairs :: [a] -> [b] -> [(a,b)]
+allPairs [] _ = []
+allPairs (x:xs) ys = map ((,) x) ys ++ allPairs xs ys
+
+
